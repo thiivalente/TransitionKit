47fb1eb533c3002111feaa42babd468d864ab67e
Thiago Valente <thiagoavalente@icloud.com>
2019-09-13 00:51:15 -0300

    Improved nomenclature of function and variables, and added to transtion type type string and iterable

--- TransitionKit/Sources/Models/Transition.swift
+++ TransitionKit/Sources/Models/Transition.swift
@@ -10,39 +10,39 @@ import UIKit
 
 public class Transition{
     
-    private var typeTransition: TransitionType
-    private var transitionTime: Double = 0.5
+    private var type: TransitionType
+    private var time: Double = 0.5
 
     /// Needed to use the framework.
     ///
     /// - Parameters:
-    ///   - typeTransition: Choose your transition
-    ///   - transitionTime: Choose the time of transition
-    public init(typeTransition: TransitionType,transitionTime: Double? = nil) {
-        self.typeTransition = typeTransition
-        self.transitionTime = (transitionTime == nil) ? 0.5 : transitionTime!
+    ///   - type: Choose your transition
+    ///   - time: Choose the time of transition
+    public init(type: TransitionType, time: Double = 0.5) {
+        self.type = type
+        self.time = time
     }
 
     /// This function can change the time after the initialize
     ///
     /// - Parameter time: Double
-    public func changeTime(time: Double) {
-        self.transitionTime = time
+    public func change(time: Double) {
+        self.time = time
     }
 
     /// If you want to use multiple transitions
     ///
-    /// - Parameter typeTransition: Choose the new transtion
-    public func changeType(typeTransition: TransitionType) {
-        self.typeTransition = typeTransition
+    /// - Parameter type: Choose the new transtion
+    public func change(type: TransitionType) {
+        self.type = type
     }
 
     /// The transition selected will be return the effect in this call
     ///
     /// - Parameter side: Choose your side transition effect
     /// - Returns: Return the effect of transtion
-    public func runTransition(side: TransitionMoviment) -> UIViewControllerAnimatedTransitioning {
-        switch self.typeTransition {
+    public func run(side: TransitionMoviment = .left) -> UIViewControllerAnimatedTransitioning {
+        switch type {
         case .fade:
             return FadeAnimationController()
         case .slide:
--- TransitionKit/Sources/Models/TransitionType.swift
+++ TransitionKit/Sources/Models/TransitionType.swift
@@ -10,31 +10,12 @@ import Foundation
 
 // This enum will inform the animation controller which animation class was selected based on the option that the user selected
 
-public enum TransitionType {
-    case fade
-    case slide
-    case slideFade
-    case flip
-    case story
-
-    // This init is optionional, but recommended to select the option based on the tag of UISwitch
-    public init?(rawValue: Int){
-        switch rawValue {
-        case 0:
-            self = .fade
-        case 1:
-            self = .slide
-        case 2:
-            self = .slideFade
-        case 3:
-            self = .flip
-        case 4:
-            self = .story
-            
-        default:
-            return nil
-        }
-    }
+public enum TransitionType: String, CaseIterable {
+    case fade = "Fade"
+    case slide = "Slide"
+    case slideFade = "Slide & Fade"
+    case flip = "Flip"
+    case story = "Story"
 }
 
 public enum TransitionMoviment{
